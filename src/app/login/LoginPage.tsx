deca5394d388989b5a6107f006d8a27af4339e07
anuka <123344+Anubisss@users.noreply.github.com>
2025-05-12 13:09:51 +0200

    Add dark theme for 2 missing pages (login, system selection)

--- src/app/login/LoginPage.tsx
+++ src/app/login/LoginPage.tsx
@@ -45,29 +45,37 @@ const Login = () => {
   const showLoadingIndicator = isFetchingDeviceStates || isLoggingIn;
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-sm rounded-md bg-white p-6 shadow-md">
-        <h2 className="mb-6 text-center text-2xl font-semibold text-gray-700">Login</h2>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <div className="w-full max-w-sm rounded-md bg-white p-6 shadow-md dark:bg-gray-800">
+        <h2 className="mb-6 text-center text-2xl font-semibold text-gray-700 dark:text-gray-400">
+          Login
+        </h2>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600" htmlFor="email">
+            <label
+              className="block text-sm font-medium text-gray-600 dark:text-gray-500"
+              htmlFor="email"
+            >
               Email Address
             </label>
             <input
               type="email"
-              className="mt-1 w-full rounded-md border bg-gray-200 px-3 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring"
+              className="mt-1 w-full rounded-md border bg-gray-200 px-3 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-900"
               placeholder="Enter your Zipato email"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
             />
           </div>
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600" htmlFor="password">
+            <label
+              className="block text-sm font-medium text-gray-600 dark:text-gray-500"
+              htmlFor="password"
+            >
               Password
             </label>
             <input
               type="password"
-              className="mt-1 w-full rounded-md border bg-gray-200 px-3 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring"
+              className="mt-1 w-full rounded-md border bg-gray-200 px-3 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-900"
               placeholder="Enter your password"
               onChange={(e) => setPassword(e.target.value)}
               value={password}
@@ -86,7 +94,7 @@ const Login = () => {
           {!showLoadingIndicator && (
             <button
               type="submit"
-              className="w-full rounded-md bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full rounded-md bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600 dark:bg-blue-800 dark:text-gray-300 dark:hover:bg-blue-700"
             >
               Login
             </button>
--- src/app/systems/page.tsx
+++ src/app/systems/page.tsx
@@ -21,9 +21,11 @@ const Systems = () => {
   const showLoadingIndicator = isFetching || isSelectingSystem || !systems;
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
-      <div className="mx-auto max-w-7xl rounded-lg bg-white p-6 shadow-lg">
-        <h2 className="mb-6 text-center text-2xl font-semibold text-gray-700">Select a System</h2>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4 dark:bg-gray-900">
+      <div className="mx-auto max-w-7xl rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800">
+        <h2 className="mb-6 text-center text-2xl font-semibold text-gray-700 dark:text-gray-400">
+          Select a System
+        </h2>
         {showLoadingIndicator && (
           <div className="flex items-center justify-center">
             <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-blue-500"></div>
@@ -35,13 +37,18 @@ const Systems = () => {
         {!showLoadingIndicator && !error && (
           <div className="flex flex-wrap justify-center gap-6">
             {systems.map((system) => (
-              <div key={system.uuid} className="rounded-lg bg-gray-200 p-4 shadow-md">
-                <h3 className="text-lg font-semibold text-gray-800">{system.name}</h3>
-                <p className="text-sm">{system.owner}</p>
-                <p className="text-sm">{system.uuid}</p>
+              <div
+                key={system.uuid}
+                className="rounded-lg bg-gray-200 p-4 shadow-md dark:bg-gray-700"
+              >
+                <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-400">
+                  {system.name}
+                </h3>
+                <p className="text-sm dark:text-gray-500">{system.owner}</p>
+                <p className="text-sm dark:text-gray-500">{system.uuid}</p>
                 <div className="mt-4 text-right">
                   <button
-                    className="rounded bg-blue-500 px-4 py-2 text-white transition-colors duration-200 hover:bg-blue-700"
+                    className="rounded bg-blue-500 px-4 py-2 text-white transition-colors duration-200 hover:bg-blue-700 dark:bg-blue-800 dark:text-gray-300 dark:hover:bg-blue-600"
                     onClick={() => handleSystemSelect(system.uuid)}
                   >
                     Select
